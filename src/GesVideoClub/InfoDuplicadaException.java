c44ea0b80a53532d35905f5dd82dc7615fc85cff
Pamela Leiva <geleivago@est.utn.ac.cr>
2023-12-04 15:13:36 -0600

    Clase Categoria
    Sea agrega la clase Categoria y Excepciones Personalizadas

---
+++ src/GesVideoClub/InfoDuplicadaException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GesVideoClub;
+
+/**
+ *
+ * @author gollo
+ */
+public class InfoDuplicadaException extends Exception {
+
+    public InfoDuplicadaException() {
+        super("Ocurrio un error debido a que hay informacion duplicada");
+    }
+
+}
